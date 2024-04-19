24c8b93929a7287d5a1e85c933eb96762168e9b9
dreamqip <v.chuchupak@gmail.com>
2024-04-19 23:32:38 +0200

    fix: build errors

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -24,7 +24,7 @@
 				<a href="{base}/about">About</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+				<a href="{base}/sverdle">Sverdle</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
